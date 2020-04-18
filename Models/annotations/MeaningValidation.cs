ffe68096d4e47d9679ebcbb69b6072ecf163977b
yanitrix <dominiix.2001@gmail.com>
2020-04-18 12:39:57 +0200

    Validation for Meaning class

---
+++ Models/annotations/MeaningValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dictionary_MVC.Models.annotations
+{
+    public class MeaningValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var meaning = (Meaning)validationContext.ObjectInstance;
+
+            if (String.IsNullOrEmpty(meaning.Value) && String.IsNullOrEmpty(meaning.Example))
+                return new ValidationResult("Either example or meaning must be present");
+            
+            return ValidationResult.Success;
+        }
+    }
+}
