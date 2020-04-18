08c5410b94e82d6f7e36b129c534861575f40992
yanitrix <dominiix.2001@gmail.com>
2020-04-18 12:23:46 +0200

    Changes in data model
    I think that the basic data model is actually finished and ready to go

--- Models/Dictionary.cs
+++ Models/Dictionary.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,17 @@ namespace Dictionary_MVC.Models
 {
     public class Dictionary
     {
+        [ForeignKey("LanguageIn")]
+        public int LanguageInID { get; set; }
+        [Required]
         public Language LanguageIn { get; set; }
 
+        [ForeignKey("LanguageOut")]
+        public int LanguageOutID { get; set; }
+        [Required]
         public Language LanguageOut { get; set; }
 
         public ISet<Entry> Entries { get; set; }
+
     }
 }
--- Models/Word.cs
+++ Models/Word.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,11 +10,19 @@ namespace Dictionary_MVC.Models
 {
     public class Word : IComparable<Word>
     {
+        public int ID { get; set; }
+
+        [Required]
         public Language SourceLanguage { get; set; }
+        [ForeignKey("SourceLanguageID")]
+        public int SourceLanguageID { get; set; }
 
         [Required]
         public String Value { get; set; }
 
+        public ICollection<Meaning> Meanings { get; set; }
+
+        [Required]
         public WordProperties Properties { get; set; }
 
         public int CompareTo([AllowNull] Word other)
