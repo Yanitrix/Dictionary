5995fe8b04625bb298cb18006abc28f77e81b62f
yanitrix <dominiix.2001@gmail.com>
2020-04-21 22:41:15 +0200

    Refactored attributes/properties
    Refactored in such a way it should be understandable and not so complicated

---
+++ Models/speech properties/SpeechPartProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dictionary_MVC.Models
+{
+    public class SpeechPartProperty
+    {
+        public int ID { get; set; }
+
+        public SpeechPart SpeechPart { get; set; }
+
+        public String Name { get; set; }
+
+        [NotMapped]
+        public String DefaultValue
+        {
+            get { return PossibleValues[0]; }
+            //set { PossibleValues[0] = value; }
+        }
+
+        /// <summary>
+        /// First value of the list is the default value for a Property.
+        /// </summary>
+        public IList<String> PossibleValues { get; set; } = new List<String>();
+
+    }
+}
--- Models/speech properties/WordProperty.cs
+++ Models/speech properties/WordProperty.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Dictionary_MVC.Models
 {
-    public class SpeechPart
+    public class WordProperty
     {
         public int ID { get; set; }
 
         public String Name { get; set; }
 
-        public ICollection<SpeechPartProperty> Properties { get; set; }
+        public String Value { get; set; }
     }
 }
