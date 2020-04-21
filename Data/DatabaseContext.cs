58858552e8e8671bf43efdef48460b6c44d1e0b3
yanitrix <dominiix.2001@gmail.com>
2020-04-21 22:47:47 +0200

    Changed "ApplicationDbContext" to "DatabaseContext"
    ^ + refactoring of the name where needed, basic fluent api calls in dbcontext

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dictionary_MVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Dictionary_MVC.Data
+{
+    public class DatabaseContext : IdentityDbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Dictionary>().HasKey(d => new { d.LanguageInID, d.LanguageOutID });
+
+
+            //other stuff
+            builder.Entity<SpeechPartProperty>().Property(p => p.PossibleValues).
+                HasConversion(
+                list => JsonConvert.SerializeObject(list),
+                list => JsonConvert.DeserializeObject<List<String>>(list));
+        }
+    }
+}
--- Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dictionary_MVC.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(DatabaseContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
