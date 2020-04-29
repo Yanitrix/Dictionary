f1aae370cc5e603c65317e2bea17d37235685b68
yanitrix <dominiix.2001@gmail.com>
2020-04-29 12:20:52 +0200

    moved annotations out of models directory

--- Dictionary MVC/Models/Meaning.cs
+++ Dictionary MVC/Models/Meaning.cs
@@ -1,4 +1,4 @@
-﻿using Dictionary_MVC.Models.annotations;
+﻿using Dictionary_MVC.Metadata.Annotations;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
