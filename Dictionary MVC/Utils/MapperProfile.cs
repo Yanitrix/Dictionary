032d0ed00bef758a743b1ec42b540a3fd9cd8c81
yanitrix <dominiix.2001@gmail.com>
2020-05-14 20:56:28 +0200

    mapper config
    mapper profile that for now does nothing and extension for adding mapper to IServiceCollection

---
+++ Dictionary MVC/Utils/MapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Dictionary_MVC.Models;
+
+namespace Dictionary_MVC.Utils
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile() 
+        {
+            //CreateMap<LanguageDto, Language>()
+        }
+    }
+}
---
+++ Dictionary MVC/Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dictionary_MVC.Utils
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAutoMapper(this IServiceCollection services)
+        {
+            var congif = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MapperProfile());
+            });
+
+            IMapper mapper = congif.CreateMapper();
+            return services.AddSingleton(mapper);
+        }
+    }
+}
