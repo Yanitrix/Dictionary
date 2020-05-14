57f40c74392ac246c3cba70a76d5298d207963f3
yanitrix <dominiix.2001@gmail.com>
2020-05-14 20:53:44 +0200

    cerated dtos for domain objects

---
+++ Api/Dto/DictionaryDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Api.Dto
+{
+    public class DictionaryDto
+    {
+        public int Index { get; set; }
+
+        [Required]
+        public String LanguageInName { get; set; }
+
+        [Required]
+        public String LanguageOutName { get; set; }
+
+        [Required]
+        public ICollection<EntryDto> Entries { get; set; }
+    }
+}
---
+++ Api/Dto/EntryDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Api.Dto
+{
+    public class EntryDto
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public int DictionaryIndex { get; set; }
+
+        [Required]
+        public int WordID { get; set; }
+
+        [Required]
+        public ISet<MeaningDto> Meanings { get; set; }
+
+        public ISet<ExpressionDto> Expressions { get; set; } = new HashSet<ExpressionDto>();
+    }
+}
