6516e33eda75a77f6b90e15672f44073a2511bda
yanitrix <dominiix.2001@gmail.com>
2020-06-01 21:25:11 +0200

    services implementation

--- Api/Service/LanguageService.cs
+++ Api/Service/LanguageService.cs
@@ -1,46 +1,46 @@
-﻿using Dictionary_MVC.Data;
+﻿using Api.Dto;
+using Dictionary_MVC.Data;
 using Dictionary_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Api.Service
 {
 
-    public class LanguageService : ServiceBase<Language>
+    public class LanguageService : ServiceBase<Language>, ILanguageService
     {
         public LanguageService(DatabaseContext context) : base(context) { }
 
+        public Language GetByName(string name)
+        {
+            return GetOne(x => x.Name == name);
+        }
+
+        public override bool IsReadyToAdd(Language entity)
+        {
+            var name = entity.Name;
+            if (context.Languages.Any(x => x.Name == name))
+            {
+                ValidationDictionary.AddError(nameof(entity.Name), $"Language with name {name} already exists in the database");
+                return false;
+            }
+
+            return true;
+        }
+
+        public override bool IsReadyToUpdate(Language entity)
+        {
+            throw new NotImplementedException();
+        }
+
         public override bool IsValid(Language entity)
         {
-            //custom validation
-            return false;
-        }
-
-        public override Language Create(Language entity)
-        {
             throw new NotImplementedException();
         }
 
-        public override Language GetOne(Func<Language, bool> condition)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IEnumerable<Language> Get(Func<Language, bool> condition)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Language Update(Language entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Language Delete(Language entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
