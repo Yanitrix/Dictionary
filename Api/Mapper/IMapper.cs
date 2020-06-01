79646c581709e6e78a2f191d128c930bba380bee
yanitrix <dominiix.2001@gmail.com>
2020-06-01 19:46:33 +0200

    added mapper because i need it
    , maybe will change to automapper when im able to configure it correctly

---
+++ Api/Mapper/IMapper.cs
@@ -0,0 +1,23 @@
+﻿using Api.Dto;
+using Dictionary_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Mapper
+{
+    public interface IMapper
+    {
+        public Language Map(LanguageDto dto);
+
+        public LanguageDto Map(Language language);
+
+        public Word Map(WordDto dto);
+
+        public WordDto Map(Word word);
+
+        public SpeechPart Map(SpeechPartDto dto);
+
+        public SpeechPartDto Map(SpeechPart speechPart);
+    }
+}
