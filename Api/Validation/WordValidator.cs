6dd8cec4a39c21ce9071fed22a4aa4cf9ee98c44
yanitrix <dominiix.2001@gmail.com>
2020-07-23 12:40:05 +0200

    new validators ang changes in the old ones
    constants instead of empty strings, collections should be posted empty, then subitem posted with correct foreign key

---
+++ Api/Validation/WordValidator.cs
@@ -0,0 +1,25 @@
+﻿using Commons;
+using Dictionary_MVC.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Validation
+{
+    public class WordValidator : AbstractValidator<Word>
+    {
+        public WordValidator()
+        {
+            RuleFor(w => w.ID).Empty().WithMessage(MessageConstants.EMPTY_ID);
+            //TODO
+            //RuleFor(w => w.SourceLanguageName).Matches("^[a-zA-Z]+$");    won't check it because service will check if the language actually exists in database
+            //RuleFor(w => w.SpeechPartName)                                //wont check it, same reason as above
+            //RuleForEach(w => w.Properties)                                ------------ '' ------------
+
+            RuleFor(w => w.Value).NotEmpty().Matches(RegexConstants.ALPHA_REGEX);
+
+        }
+    }
+}
