d7e59b2e68c9d1e4d1c415d56368a9c9fb69ebb4
yanitrix <dominiix.2001@gmail.com>
2020-07-24 18:51:59 +0200

    expression foreign keys made nullable, migration made

---
+++ Data/Data/Migrations/20200724144739_ExpressionFKeyNullable.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dictionary_MVC.Data.Migrations
+{
+    public partial class ExpressionFKeyNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expression_Dictionary_DictionaryIndex",
+                table: "Expression");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expression_Meaning_MeaningID",
+                table: "Expression");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MeaningID",
+                table: "Expression",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DictionaryIndex",
+                table: "Expression",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expression_Dictionary_DictionaryIndex",
+                table: "Expression",
+                column: "DictionaryIndex",
+                principalTable: "Dictionary",
+                principalColumn: "Index",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expression_Meaning_MeaningID",
+                table: "Expression",
+                column: "MeaningID",
+                principalTable: "Meaning",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expression_Dictionary_DictionaryIndex",
+                table: "Expression");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expression_Meaning_MeaningID",
+                table: "Expression");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MeaningID",
+                table: "Expression",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DictionaryIndex",
+                table: "Expression",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expression_Dictionary_DictionaryIndex",
+                table: "Expression",
+                column: "DictionaryIndex",
+                principalTable: "Dictionary",
+                principalColumn: "Index",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expression_Meaning_MeaningID",
+                table: "Expression",
+                column: "MeaningID",
+                principalTable: "Meaning",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
