2740e0aac08118e39a330a543a1aec2090d536ce
yanitrix <dominiix.2001@gmail.com>
2020-08-04 18:50:39 +0200

    speech part propety service implementation

---
+++ Api/Service/SpeechPartPropertyService.cs
@@ -0,0 +1,43 @@
+﻿using Api.Validation;
+using Dictionary_MVC.Data;
+using Dictionary_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Api.Service
+{
+    public class SpeechPartPropertyService : ServiceBase<SpeechPartProperty>, ISpeechPartPropertyService
+    {
+
+        public SpeechPartPropertyService(DatabaseContext context, SpeechPartPropertyValidator validator):base(context, validator) { }
+
+        public SpeechPartProperty GetByID(int id)
+        {
+            return repo.Find(id);
+        }
+
+        public override bool IsReadyToAdd(SpeechPartProperty entity)
+        {
+            if (!IsValid(entity)) return false;
+
+            //check if db contains speechpart
+            var speechPartIndb = context.Set<SpeechPart>().FirstOrDefault(sp => sp.Index == entity.SpeechPartIndex);
+            if (speechPartIndb == null)
+            {
+                ValidationDictionary.AddError("Speech part not found", $"Speech part with given " +
+                    $"index: \"{entity.SpeechPartIndex}\" doesn't exist in the database");
+                return false;
+            }
+
+            return true;
+        }
+
+        public override bool IsReadyToUpdate(SpeechPartProperty entity)
+        {
+            //same checks, will do the same
+            return IsReadyToAdd(entity);
+        }
+    }
+}
