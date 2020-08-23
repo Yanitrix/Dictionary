21b2201c17ae4858190c4beee728dad073d92ef0
yanitrix <dominiix.2001@gmail.com>
2020-08-24 00:10:24 +0200

    solution file and startup project updated

--- Dictionary MVC/Controllers/HomeController.cs
+++ Dictionary MVC/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Data.Models;
+using Dictionary_MVC.Models;
 
 namespace Dictionary_MVC.Controllers
 {
