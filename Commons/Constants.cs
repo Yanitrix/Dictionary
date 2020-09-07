19f4d7f7a6ac2c65f037a2ebc6b6f6eebfc5d2fa
yanitrix <dominiix.2001@gmail.com>
2020-09-07 13:04:30 +0200

    So i added a custom validation rule
    that states that a word cannot match certain regex.

--- Commons/Constants.cs
+++ Commons/Constants.cs
@@ -4,8 +4,13 @@ namespace Commons
 {
     public static class RegexConstants
     {
+        [Obsolete]
         public const String ALPHA_REGEX = "";
+        [Obsolete]
         public const String ONE_WORD_REGEX = "^[a-zA-Z]+$";
+
+        public const String ANY_DIGIT = "\\d";
+        public const String ANY_SPACE = "\\s";
     }
 
     public static class MessageConstants
@@ -14,5 +19,8 @@ namespace Commons
         public const String EMPTY_INDEX = "The Index field must be empty";
         public const String EMPTY = "Must be empty";
         public const String NOT_EMPTY = "Cannot be empty";
+
+        public const String NO_DIGIT_MESSAGE = "The literal cannot contain any digits";
+        public const String NO_SPACE_MESSAGE = "The literal cannot contain any space characets";
     }
 }
