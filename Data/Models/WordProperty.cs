3d998a92be2f75d0548e13fe6ecfb5b8b1e82b6d
yanitrix <dominiix.2001@gmail.com>
2020-09-14 13:10:02 +0200

    Removed SpeechPart and SpeechPartProperty.
    Also moved WordProperty out of the separate folder.

--- Data/Models/WordProperty.cs
+++ Data/Models/WordProperty.cs
@@ -20,11 +20,11 @@ namespace Data.Models
         public String Name { get; set; }
 
         [Required]
-        public String Value { get; set; }
+        public IList<String> Values;
 
         public override string ToString()
         {
-            return $"Name: {Name} \t Value: {Value}\n";
+            return $"Name: {Name} \t Value: {Values.Aggregate("", (left, right) => left + right)}\n";
         }
     }
 }
