ef355ebedfe4cce7b290ea3b30739c81ae5abb3e
yanitrix <dominiix.2001@gmail.com>
2020-09-17 19:12:46 +0200

    basic IService interface

---
+++ Service/Service/Abstract/IService.cs
@@ -0,0 +1,16 @@
+﻿using Api.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Service
+{
+    public interface IService<T> 
+    {
+        public ValidationDictionary TryAdd(T entity);
+        
+        public ValidationDictionary TryUpdate(T entity);
+
+        public bool IsValid(T entity);
+    }
+}
