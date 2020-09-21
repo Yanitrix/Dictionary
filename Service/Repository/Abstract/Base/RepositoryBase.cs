f67a1cf5d4d6389e7b19b2ea417fb4ec7754687c
yanitrix <dominiix.2001@gmail.com>
2020-09-21 12:05:10 +0200

    changed method order in file, made constructor protected instead of public
    also added Deatch() method to detach an entity.

--- Service/Repository/Abstract/Base/RepositoryBase.cs
+++ Service/Repository/Abstract/Base/RepositoryBase.cs
@@ -34,52 +34,10 @@ namespace Api.Service
         protected readonly DatabaseContext context;
         protected readonly DbSet<T> repo;
 
-        public RepositoryBase(DatabaseContext context)
+        protected RepositoryBase(DatabaseContext context)
         {
             this.context = context;
             repo = context.Set<T>();
-        }
-
-        public IEnumerable<T> All()
-        {
-            return repo.ToList();
-        }
-
-        public virtual T Create(T entity)
-        {
-            repo.Add(entity);
-            context.SaveChanges();
-            return entity;
-        }
-
-        public virtual void CreateRange(params T[] entities)
-        {
-            repo.AddRange(entities);
-            context.SaveChanges();
-        }
-
-        public virtual T GetOne(Expression<Func<T, bool>> condition)
-        {
-            return repo.FirstOrDefault(condition);
-        }
-
-        public virtual IEnumerable<R> Get<R>(Expression<Func<T, bool>> condition, Expression<Func<T, R>> mapper)
-        {
-            return repo.Where(condition).Select(mapper).ToList();
-        }
-
-        public virtual T Update(T entity)
-        {
-            repo.Update(entity);
-            context.SaveChanges();
-            return entity;
-        }
-
-        public virtual T Delete(T entity)
-        {
-            repo.Remove(entity);
-            context.SaveChanges();
-            return entity;
         }
 
         protected T GetOne(Expression<Func<T, bool>> condition, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
@@ -111,7 +69,7 @@ namespace Api.Service
 
             query = query.Where(condition);
 
-            if(orderBy != null)
+            if (orderBy != null)
             {
                 return orderBy(query).Select(mapper);
             }
@@ -137,5 +95,50 @@ namespace Api.Service
             return query.ToList();
         }
 
+        public void Detach(T entity)
+        {
+            context.Entry(entity).State = EntityState.Detached;
+        }
+        public virtual T Create(T entity)
+        {
+            repo.Add(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public virtual void CreateRange(params T[] entities)
+        {
+            repo.AddRange(entities);
+            context.SaveChanges();
+        }
+
+        public virtual IEnumerable<T> All()
+        {
+            return repo.ToList();
+        }
+
+        public virtual T GetOne(Expression<Func<T, bool>> condition)
+        {
+            return repo.FirstOrDefault(condition);
+        }
+
+        public virtual IEnumerable<R> Get<R>(Expression<Func<T, bool>> condition, Expression<Func<T, R>> mapper)
+        {
+            return repo.Where(condition).Select(mapper).ToList();
+        }
+
+        public virtual T Update(T entity)
+        {
+            repo.Update(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public virtual T Delete(T entity)
+        {
+            repo.Remove(entity);
+            context.SaveChanges();
+            return entity;
+        }
     }
 }
