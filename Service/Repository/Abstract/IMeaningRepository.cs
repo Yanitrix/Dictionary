a8840536dacc52b1186bd6e718394c0f8c159bdd
yanitrix <dominiix.2001@gmail.com>
2020-09-23 21:54:28 +0200

    added methods to interface, implemented them
    and tested

--- Service/Repository/Abstract/IMeaningRepository.cs
+++ Service/Repository/Abstract/IMeaningRepository.cs
@@ -8,5 +8,11 @@ namespace Api.Service
     public interface IMeaningRepository : IRepository<Meaning>
     {
         public Meaning GetByID(int id);
+
+        public Meaning GetByIDWithEntry(int id);
+
+        public IEnumerable<Meaning> GetByValueSubstring(String value);
+
+        public IEnumerable<Meaning> GetByNotesSubstring(String notes);
     }
 }
--- Service/Repository/MeaningRepository.cs
+++ Service/Repository/MeaningRepository.cs
@@ -1,20 +1,47 @@
 ﻿using Data.Database;
 using Data.Models;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Api.Service
 {
     public class MeaningRepository : RepositoryBase<Meaning>, IMeaningRepository
     {
-
+        //All queries loaded with Examples
         public MeaningRepository(DatabaseContext context):base(context) { }
-        
+
+        private readonly Func<IQueryable<Meaning>, IIncludableQueryable<Meaning, object>> includeExamples =
+            (meanings => meanings.Include(m => m.Examples));
+        private readonly Func<IQueryable<Meaning>, IIncludableQueryable<Meaning, object>> includeExamplesAndEntry =
+            (meanings => meanings.Include(m => m.Examples).Include(m => m.Entry));
+
         public Meaning GetByID(int id)
         {
-            return repo.Find(id);
+            return GetOne(m => m.ID == id, null, includeExamples);
+        }
+
+        public Meaning GetByIDWithEntry(int id)
+        {
+            return GetOne(m => m.ID == id, null, includeExamplesAndEntry);
+        }
+
+        //includes Entry
+        public IEnumerable<Meaning> GetByValueSubstring(string value)
+        {
+            if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value)) return Enumerable.Empty<Meaning>();
+            return Get(m => m.Value.Contains(value), x => x, null, includeExamplesAndEntry);
+        }
+
+        //includes Entry
+        public IEnumerable<Meaning> GetByNotesSubstring(string notes)
+        {
+            if (String.IsNullOrEmpty(notes) || String.IsNullOrWhiteSpace(notes)) return Enumerable.Empty<Meaning>();
+            return Get(m => m.Notes.Contains(notes), x => x, null, includeExamplesAndEntry);
         }
 
         public override Meaning Delete(Meaning entity) //Examples must be included
@@ -23,6 +50,5 @@ namespace Api.Service
             base.Delete(entity);
             return entity;
         }
-
     }
 }
