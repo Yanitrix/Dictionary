16a99f82d5927b5be85510c6f21bbf2f8dc447fb
yanitrix <dominiix.2001@gmail.com>
2020-10-01 14:24:46 +0200

    tested cascade deleting
    , everything works on inmemorydb, do some integration tests using sqlserver

--- Data.Tests/DbContextTestBase.cs
+++ Data.Tests/DbContextTestBase.cs
@@ -8,9 +8,19 @@ namespace Data.Tests
 {
     public class DbContextTestBase : IDisposable
     {
-        protected readonly DatabaseContext context;
+        protected DatabaseContext context;
 
         public DbContextTestBase()
+        {
+            initializeDatabase();
+        }
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+
+        protected void initializeDatabase()
         {
             var options = new DbContextOptionsBuilder<DatabaseContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
@@ -18,11 +28,6 @@ namespace Data.Tests
             this.context = new DatabaseContext(options);
 
             context.Database.EnsureCreated();
-        }
-        public void Dispose()
-        {
-            context.Database.EnsureDeleted();
-            context.Dispose();
         }
     }
 }
