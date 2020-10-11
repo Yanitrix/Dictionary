dc34ec4c8c2b7db14141eb461d63f95f599224f5
yanitrix <dominiix.2001@gmail.com>
2020-10-11 20:43:10 +0200

    changed one test, added test class for 1 method.
    didnt create another project because its useless to do it just for 1 test class

---
+++ Service.Tests/Constants/NotFoundDescTests.cs
@@ -0,0 +1,45 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using msg = Commons.ValidationErrorMessages;
+
+namespace Service.Tests.Constants
+{
+    public class NotFoundDescTests
+    {
+        [Fact]
+        public void NotFoundDescWithEntry()
+        {
+            var entry = new Entry
+            {
+                DictionaryIndex = 12,
+                WordID = 5
+            };
+
+            var dictMsg = $"Dictionary with given Index: {entry.DictionaryIndex} was not found in the database. Create it before posting a(n) Entry";
+            var wordMsg = $"Word with given ID: {entry.WordID} was not found in the database. Create it before posting a(n) Entry";
+
+            var actualDictMsg = msg.NOTFOUND_DESC<Entry, Dictionary, int>(d => d.Index, entry.DictionaryIndex);
+            var actualWordMsg = msg.NOTFOUND_DESC<Entry, Word, int>(w => w.ID, entry.WordID);
+
+            Assert.Equal(dictMsg, actualDictMsg);
+            Assert.Equal(wordMsg, actualWordMsg);
+        }
+
+        [Fact]
+        public void NotFoundWihtWord()
+        {
+            var word = new Word
+            {
+                SourceLanguageName = "posli",
+            };
+
+            var expectedMsg = $"Language with given Name: {word.SourceLanguageName} was not found in the database. Create it before posting a(n) Word";
+            var actualMsg = msg.NOTFOUND_DESC<Word, Language, String>(l => l.Name, word.SourceLanguageName);
+
+            Assert.Equal(expectedMsg, actualMsg);
+        }
+    }
+}
