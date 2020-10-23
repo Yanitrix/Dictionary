06abbbcab55f94dfa329d3a4a9ad03b2be45daf4
yanitrix <dominiix.2001@gmail.com>
2020-10-23 14:40:30 +0200

    Added custom collections and custom comparators

---
+++ Data/Custom collections/WordPropertySet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Data.Models
+{
+    public class WordPropertySet : HashSet<WordProperty>
+    {
+        public WordPropertySet():base(new WordPropertyEqualityComparer())
+        {
+        }
+    }
+}
