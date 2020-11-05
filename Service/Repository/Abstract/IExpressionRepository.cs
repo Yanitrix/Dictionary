bf0cd30d35a67dbd1c07c0533bb98c9e6806e0a0
yanitrix <dominiix.2001@gmail.com>
2020-11-05 13:17:16 +0100

    Added string finding methods that need dictionary

--- Service/Repository/Abstract/IExpressionRepository.cs
+++ Service/Repository/Abstract/IExpressionRepository.cs
@@ -11,5 +11,19 @@ namespace Service.Repository
         public IEnumerable<Expression> GetByTextSubstring(String text);
 
         public IEnumerable<Expression> GetByTranslationSubstring(String translation);
+
+        /// <summary>
+        /// Retrieves a collection of Expressions with similar Text and given Dictionary.Index
+        /// </summary>
+        /// <param name="dictionaryIndex"></param>
+        /// <param name="textSubstring"></param>
+        public IEnumerable<Expression> GetByDictionaryTextSubstring(int dictionaryIndex, string textSubstring);
+
+        /// <summary>
+        /// Retrieves a collection of Expressions with similar Translation and given Dictionary.Index
+        /// </summary>
+        /// <param name="dictionaryIndex"></param>
+        /// <param name="translationSubstring"></param>
+        public IEnumerable<Expression> GetByDictionaryTranslationSubstring(int dictionaryIndex, string translationSubstring);
     }
 }
--- Service/Repository/Abstract/IMeaningRepository.cs
+++ Service/Repository/Abstract/IMeaningRepository.cs
@@ -17,16 +17,22 @@ namespace Service.Repository
         /// Including Entry
         /// </summary>
         /// <param name="value"></param>
-        /// <returns></returns>
         public IEnumerable<Meaning> GetByValueSubstring(String value);
 
         /// <summary>
         /// Including Entry
         /// </summary>
         /// <param name="notes"></param>
-        /// <returns></returns>
         public IEnumerable<Meaning> GetByNotesSubstring(String notes);
 
+        /// <summary>
+        /// Retrieves a collection of Meanings with similar value and given Dictionary.Index.
+        /// Includes Entry
+        /// </summary>
+        /// <param name="dictionaryIndex"></param>
+        /// <param name="valueSubstring"></param>
+        public IEnumerable<Meaning> GetByDictionaryAndValueSubstring(int dictionaryIndex, String valueSubstring);
+        
         public bool ExistsByID(int id);
     }
 }
