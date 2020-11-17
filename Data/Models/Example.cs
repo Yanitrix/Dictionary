41aa4cead490fb5a0a7e20055c8a87c31ea59a26
yanitrix <dominiix.2001@gmail.com>
2020-11-17 21:20:21 +0100

    Added inheritance hierarchy.

--- Data/Models/Example.cs
+++ Data/Models/Example.cs
@@ -3,15 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
 {
-    public class Example
+    public class Example : Expression
     {
-        public int ID { get; set; }
-
-        [Required]
-        public String Text { get; set; }
-        [Required]
-        public String Translation { get; set; }
-
         public int MeaningID { get; set; }
         public Meaning Meaning { get; set; }
     }
--- Data/Models/FreeExpression.cs
+++ Data/Models/FreeExpression.cs
@@ -3,15 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
 {
-    public class FreeExpression
+    public class FreeExpression : Expression
     {
-        public int ID { get; set; }
-
-        [Required]
-        public String Text { get; set; }
-        [Required]
-        public String Translation { get; set; }
-
         public Dictionary Dictionary { get; set; }
         public int DictionaryIndex { get; set; }
     }
