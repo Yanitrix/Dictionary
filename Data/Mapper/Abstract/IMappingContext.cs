620e961739781d3c309567ba1a083932acd698c0
yanitrix <dominiix.2001@gmail.com>
2020-12-15 12:31:13 +0100

    Implemented custom mapper in a simple way.
    Added a dependency injection extension.

---
+++ Data/Mapper/Abstract/IMappingContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Data.Mapper
+{
+    public interface IMappingContext
+    {
+        public Func<T, R> ResolveMappingFunction<T, R>();
+
+        public void RegisterMapping<T, R>(Func<T, R> mappingFunction);
+    }
+}
