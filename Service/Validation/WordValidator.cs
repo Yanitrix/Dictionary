713f7a996b8dcf4058f388113d90b728a9dc4f49
yanitrix <dominiix.2001@gmail.com>
2020-12-16 11:11:44 +0100

    Resvolved bug that Properties could not be empty
    Closes #167

--- Service/Validation/WordValidator.cs
+++ Service/Validation/WordValidator.cs
@@ -9,11 +9,9 @@ namespace Service.Validation
         public WordValidator()
         {
             RuleFor(w => w.ID).Empty().WithMessage(MessageConstants.EMPTY_ID);
-
             RuleFor(w => w.SourceLanguageName).NotEmpty().WithMessage(MessageConstants.NOT_EMPTY).NoDigitsNoSpaces();
 
             RuleFor(w => w.Value).NotEmpty().NoDigits();
-            RuleFor(w => w.Properties).NotEmpty().WithMessage("Collection of WordProperties cannot be empty");
 
             RuleForEach(w => w.Properties).SetValidator(new WordPropertyValidator());
         }
