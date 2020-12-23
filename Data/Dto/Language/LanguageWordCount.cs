04a9d2a233a57d9b5eb8c211a2b0932fa4f58f39
yanitrix <dominiix.2001@gmail.com>
2020-12-23 21:56:10 +0100

    simplified property name

--- Data/Dto/Language/LanguageWordCount.cs
+++ Data/Dto/Language/LanguageWordCount.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Data.Dto
 {
     public class LanguageWordCount
     {
-        public String LanguageName { get; set; }
+        public String Name { get; set; }
 
         public int WordCount { get; set; }
     }
