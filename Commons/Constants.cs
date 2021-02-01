8f9af6cead00f8d426a7982382efe25eb2f72207
yanitrix <dominiix.2001@gmail.com>
2021-02-01 01:35:07 +0100

    Changed method names and messages a bit.

--- Commons/Constants.cs
+++ Commons/Constants.cs
@@ -22,13 +22,11 @@ namespace Commons
 
     public static class ValidationErrorMessages
     {
-        public const String DOESNT_EXIST = "Entity does not exist";
-        
         public const String DUPLICATE = "Duplicate";
         public const String DUPLICATE_LANGUAGE_DESC = "Language with same name already exists in the database. The case is ignored";
         public const String DUPLICATE_WORD_DESC = "There's already at least one Word in the database with same Value, same SourceLanguageName and same set of WordProperties";
         public const String DUPLICATE_DICTIONARY_DESC = "Dictionary of same Languages already exist in the database. The case is ignored";
-        public const String DUPLICATE_ENTRY_DESC = "An Entry for given Word already exists. If you want to add another Meaning, post it on its respective endpoint";
+        public const String DUPLICATE_ENTRY_DESC = "An Entry for given Word and Dictionary already exists. If you want to add another Meaning, post it on its respective endpoint";
         
         public const String CANNOT_UPDATE = "Entity cannot be updated";
         public const String CANNOT_UPDATE_LANGUAGE_DESC = "Name property cannot be changed. If you want to add words to a Language, post them on their respective endpoint";
@@ -37,11 +35,12 @@ namespace Commons
 
         public const String LANGUAGES_NOT_MATCH = "Language does not match";
         public const String LANGUAGES_NOT_MATCH_DESC = "SourceLanguage of the Word is different than LanguageIn of the Dictionary. The case is NOT ignored";
-        
-        public static String NOTFOUND<T>() => $"{typeof(T).Name} not found";
-        public static String DOESNT_EXIST_PK<T>() => $"{typeof(T).Name} with given primary key does not exist in the database.";
-        public static String DOESNT_EXIST_UPDATE<T>() => DOESNT_EXIST_PK<T>() + "There is nothing to update";
-        public static String NOTFOUND_DESC<A, B>(Expression<Func<B, object>> pkeyEx, object pkeyValue)
+
+        public static String EntityNotFound() => "Entity does not exist.";
+        public static String EntityNotFound<T>() => $"{typeof(T).Name} does not exist.";
+        public static String EntityDoesNotExistByPrimaryKey<T>() => $"{typeof(T).Name} with given primary key does not exist.";
+        public static String ThereIsNothingToUpdate<T>() => EntityDoesNotExistByPrimaryKey<T>() + " There is nothing to update";
+        public static String EntityDoesNotExistByForeignKey<A, B>(Expression<Func<B, object>> pkeyEx, object pkeyValue)
         {
             return pkeyEx.Body switch
             {
@@ -50,6 +49,6 @@ namespace Commons
                 _ => "null",
             };
         }
-        public static String LANGS_NOTFOUND_DESC(String langIn, String langOut) => $"Language: \"{langIn}\" or \"{langOut}\" does not exist in the database. Create them before posting a Dictionary";
+        public static String LanguagesNotFoundDesc(String langIn, String langOut) => $"Language: \"{langIn}\" or \"{langOut}\" does not exist in the database. Create them before posting a Dictionary";
     }
 }
