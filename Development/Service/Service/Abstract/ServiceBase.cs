1f68b084286a0592cc9c73dd2fa042689d34788a
yanitrix <dominiix.2001@gmail.com>
2021-02-18 11:36:26 +0100

    Removed base interface and added a base class.
    For convenience (mainly mapper)

---
+++ Development/Service/Service/Abstract/ServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Repository;
+using Service.Mapper;
+
+namespace Service.Service.Abstract
+{
+    public abstract class ServiceBase
+    {
+        protected readonly IMapper mapper;
+        protected readonly IUnitOfWork uow;
+
+        protected ServiceBase(IUnitOfWork uow, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.uow = uow;
+        }
+    }
+}
