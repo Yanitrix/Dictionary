d82d155796cc54d1d4f961816ebc05e178a4a259
yanitrix <dominiix.2001@gmail.com>
2021-04-08 21:15:32 +0200

    Translation folder in Domain (i don't rememver where it was before)

---
+++ Development/Domain/Translation/ITranslationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domain.Translation
+{
+    public interface ITranslationService
+    {
+        /// <summary>
+        /// If bidirectional is true then at least one of the dictionaries has to exist in order to return true.
+        /// </summary>
+        /// <param name="languageIn"></param>
+        /// <param name="languageOut"></param>
+        /// <param name="bidirectional"></param>
+        bool EnsureDictionaryExists(String languageIn, String languageOut, bool bidirectional = false);
+
+        TranslationResponse Translate(String dictionary, String query);
+
+        BidirectionalTranslationResponse TranslateBidir(String dictionaries, String query);
+    }
+}
---
+++ Development/Domain/Translation/TranslationResponse.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Dto;
+using System.Collections.Generic;
+
+namespace Domain.Translation
+{
+    public class TranslationResponse
+    {
+        //so a list of Entries for both Dictionaries
+        public IEnumerable<GetEntry> ResultEntries { get; set; }
+
+        public IEnumerable<GetFreeExpression> ResultExpressions { get; set; }
+    }
+}
